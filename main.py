f36cfe8e33603970de972211bae2606b83d1c036
Russell Parker <russell.parker7@gmail.com>
2017-11-17 12:29:29 -0500

    Added about and FAQ pages, footer navbar

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask (__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
