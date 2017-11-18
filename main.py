7f123f1323f6ee06f0f906e0d2b11a6c6701adba
Russell Parker <russell.parker7@gmail.com>
2017-11-18 12:44:11 -0500

    Functionality for flashing messages

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, flash
+import os
+from google.appengine.api import users
 
 app = Flask (__name__)
 
+# necessary for keeping client-side sessions secure
+app.secret_key = os.urandom(24)
+
+# check if app is running in production
+production = os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')
+
+if not production:
+    app.debug = True
+
+class Alert:
+    success = "alert-success"
+    info = "alert-info"
+    warning = "alert-warning"
+    danger = "alert-danger"
+
 @app.route('/')
 def index():
+    flash('This is a test', Alert.info)
     return render_template('index.html')
 
 @app.route('/faq')
@@ -16,8 +34,14 @@ def about():
 
 @app.route('/account/choose-account')
 def choose_account():
-    return render_template('accounts/choose-account.html')
+    return_url = url_for("index")
+    env = {"login_url": users.create_login_url(return_url)}
+    return render_template('accounts/choose-account.html', **env)
 
 @app.route('/account/secure-account')
 def secure_account():
     return render_template('accounts/secure-account.html')
+
+@app.route('/account/create-account')
+def create_account():
+    return render_template('accounts/create-account.html')
