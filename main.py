a303eaf94728d4c9cf1bbdd6857cad49f8abc4d6
Russell Parker <russell.parker7@gmail.com>
2017-11-27 18:16:36 -0500

    Many changes including login/logout env

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash
-import os
+import os, logging, email
 from google.appengine.api import users
 
 app = Flask (__name__)
@@ -19,29 +19,72 @@ class Alert:
     warning = "alert-warning"
     danger = "alert-danger"
 
+def get_account(path=None):
+    # overwrite if not specified
+    path = path or request.path
+    user = users.get_current_user()
+    account = {}
+    if not user:
+        account.update(
+            user = None,
+            login = users.create_login_url(path),
+        )
+    else:
+        account.update(
+            user = user,
+            kind = 'individual',
+            logout = users.create_logout_url(path),
+        )
+    return account
+
+
 @app.route('/')
 def index():
-    flash('This is a test', Alert.info)
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        account=get_account()
+    )
 
 @app.route('/faq')
 def faq():
-    return render_template('faq.html')
+    return render_template(
+        'faq.html',
+        account=get_account()
+    )
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template(
+        'about.html',
+        account=get_account()
+    )
 
 @app.route('/account/choose-account')
 def choose_account():
-    return_url = url_for("index")
-    env = {"login_url": users.create_login_url(return_url)}
-    return render_template('accounts/choose-account.html', **env)
+    return render_template(
+        'accounts/choose-account.html',
+        account=get_account()
+    )
 
 @app.route('/account/secure-account')
 def secure_account():
-    return render_template('accounts/secure-account.html')
+    return render_template(
+        'accounts/secure-account.html',
+        account=get_account()
+    )
 
 @app.route('/account/create-account')
 def create_account():
-    return render_template('accounts/create-account.html')
+    return render_template(
+        'accounts/create-account.html',
+        account=get_account()
+    )
+
+@app.route('/bank/<int:bank_id>')
+def bank_profile(bank_id):
+    # TODO: get bank from DB
+    return render_template(
+        'bank.html',
+        account=get_account(),
+        bank=bank,
+    )
