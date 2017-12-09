8f91312aa10a3ed3737da059569a28dcdcd296b9
Sonal Chatter <sonal.chatter@tufts.edu>
2017-12-09 17:23:52 -0500

    adding zipcode requests

--- forms/radius.py
+++ forms/radius.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, DateField, IntegerField, validators
 
 class Form(FlaskForm):
-    radius = IntegerField('Radius', validators=[
+    radius = IntegerField('Radius(mi)', validators=[
         validators.DataRequired('Radius field is required'),
-        validators.NumberRange(1, 200, 'Radius must be within valid range'),
+        validators.NumberRange(1, 200, 'Radius must be within valid range of 1 - 200'),
     ])
-    zipcode = IntegerField('Zip Code', validators=[
+    zipcode = IntegerField('Zipcode', validators=[
         validators.DataRequired('Zip code field is required'),
         validators.NumberRange(0, 99999, 'Zip code must be within valid range'),
     ])
